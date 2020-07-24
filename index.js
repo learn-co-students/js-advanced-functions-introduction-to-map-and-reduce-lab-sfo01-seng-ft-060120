e105a0b7d3a93af78ddcca71bc662032b760f94b
haleighdalke <haleighdalke@gmail.com>
2020-07-24 11:41:19 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
 // Your code here
+
+const mapToNegativize = (sourceArray) => {
+    return sourceArray.map(e => (e * -1))
+}
+
+const mapToNoChange = (sourceArray) => {
+    return sourceArray
+}
+
+const mapToDouble = (sourceArray) => {
+    return sourceArray.map(e => (e * 2))
+}
+
+const mapToSquare = (sourceArray) => {
+    return sourceArray.map(e => e **2)
+}
+
+const reduceToTotal = (sourceArray, startingPoint=0) => {
+    return sourceArray.reduce(function(total, e){ return e + total }, startingPoint)
+}
+
+const reduceToAllTrue = (sourceArray) => {
+    return sourceArray.reduce(function(total, e) {
+        return e ? true : false
+    }, 0)
+}
+
+const reduceToAnyTrue = (sourceArray) => {
+    return sourceArray.reduce(function(total, e) {
+        return e ? true : false
+    }, 0)
+}
